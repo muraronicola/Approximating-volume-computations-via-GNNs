8729d4f989ac44fba8e680b32ce556c0c3ed2761
muraronicola <nicola.muraro@studenti.unitn.it>
2025-02-25 15:39:18 +0100

    generate data done

--- GenerateData.py
+++ GenerateData.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from show_politope import *
 from scipy.optimize import linprog
 from scipy.spatial import HalfspaceIntersection, ConvexHull
+import polytope as pc
 
 def is_bounded(A, b):
     n = A.shape[1]
@@ -29,11 +30,12 @@ def generate_politope(rng, m=4, r=3):
     return is_valid, A, b
 
 
-def show_politope(A, b):
-    plot_politope(A, b)
+def load_data(filename):
+    data = np.load(filename, allow_pickle=True)
+    return data
 
 
-if __name__ == "__main__":
+def test_main():
     seed = 5
     rng = np.random.default_rng(seed)
     
@@ -42,4 +44,48 @@ if __name__ == "__main__":
     #print(A)
     #print(b)
     
-    show_politope(A, b)
+    """A = np.array([
+        [1, 0, 0],  # x <= 1
+        [-1, 0, 0], # x >= -1
+        [0, 1, 0],  # y <= 1
+        [0, -1, 0], # y >= -1
+        [0, 0, 1],  # z <= 1
+        [0, 0, -1]  # z >= -1
+        ])
+    b = np.array([1, 1, 1, 1, 1, 1])"""
+    
+    plot_politope(A, b)
+    
+    p = pc.Polytope(A, b)
+    print("Volume:", p.volume)
+
+
+def main():
+    n_politopes = 200
+    seed = 0
+    rng = np.random.default_rng(seed)
+
+    base_path = "./data/"
+    data = []
+    file = open(base_path + "politopes.txt", "w")   
+    save_images = False 
+    
+    i = 0
+    while i < n_politopes:
+        is_valid, A, b = generate_politope(rng, m=6, r=3)
+        if is_valid:
+            status = plot_politope(A, b, save=save_images, show=False, filename=base_path + "politope_" + str(i) + ".png")
+            
+            if status:
+                p = pc.Polytope(A, b)
+                data.append([A, b, p.volume])
+                i += 1
+    
+    data = np.array(data, dtype=object)
+    np.save(base_path + "politopes.npy", data)
+    file.write(str(data))
+    
+
+if __name__ == "__main__":
+    #test_main()
+    main()
--- show_politope.py
+++ show_politope.py
@@ -4,18 +4,26 @@ from scipy.spatial import HalfspaceIntersection, ConvexHull
 from scipy.optimize import linprog
 from pycvxset import Polytope, Ellipsoid, spread_points_on_a_unit_sphere
 
-def plot_politope(A, b, save=False, filename=None):
-    P_hrep_3D = Polytope(A=A, b=b)
+def plot_politope(A, b, save=False, show=True, filename=None):
+    try:
+        
+        P_hrep_3D = Polytope(A=A, b=b)
 
-    ax, _, _ = P_hrep_3D.plot(patch_args={"label": "P_hrep_3D"})
-    ax.legend()
-    ax.set_title("Plotting P_hrep_3D")
-    ax.set_xlabel("x")
-    ax.set_ylabel("y")
-    ax.set_zlabel("z")
-    
-    if not save:
-        plt.show()
-    else:
-        plt.savefig(filename)
+        ax, _, _ = P_hrep_3D.plot(patch_args={"label": "P_hrep_3D"})
+        ax.legend()
+        ax.set_title("Plotting P_hrep_3D")
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        ax.set_zlabel("z")
+        
+        if show:
+            plt.show()
+            
+        if save:
+            plt.savefig(filename)
+        
         plt.close()
+        return True
+    
+    except Exception as e:
+        return False

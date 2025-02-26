553848a746a87314f5b083990c56cb3fc82e6e8a
muraronicola <nicola.muraro@studenti.unitn.it>
2025-02-26 12:21:28 +0100

    generated dataset

--- GenerateData.py
+++ GenerateData.py
@@ -35,42 +35,59 @@ def generate_polytope(rng, m=4, r=3):
     return is_valid, A, b
 
 
-def generate_n_polytopes(n_polytopes, base_path="./data/", seed=0, m=4, r=3, save_images=True):
+def check_exact_polytope(original_volume, A, b, m): #Check if all the constraints are useful
+    for i in range(m):
+        A_i = np.delete(A, i, axis=0)
+        b_i = np.delete(b, i)
+        
+        is_valid = is_bounded(A_i, b_i)
+        if is_valid:
+            p = Polytope(A=A_i, b=b_i)
+            new_volume = p.volume()
+
+            if new_volume < original_volume + original_volume * 0.05: #The constraint is that the volume must change at least 5% to be considered a different polytope
+                return False
+        
+    return True
+
+def generate_n_polytopes(n_polytopes, base_path="./data/", seed=0, m=4, r=3, save_images=True, only_exact=False):
     rng = np.random.default_rng(seed)
 
     path = base_path + "m_" + str(m) + "_r_" + str(r) + "/"
     os.makedirs(path, exist_ok=True)
     
     data = []
+    data_exact_politope = []
     
     i = 0
-    invalids = 0
     while i < n_polytopes:
         is_valid, A, b = generate_polytope(rng, m=m, r=r)
         
         if is_valid:
             polytope = Polytope(A=A, b=b)
-            
-            if r <= 3:
-                plot_polytope(polytope, save=save_images, show=False, filename=path + "politope_" + str(i) + ".png")
-            
             volume = polytope.volume()
-            data.append([A, b, volume])
-            i += 1
-            invalids = 0
-        else:
-            invalids += 1
-            if invalids > 100:
-                return False
+            
+            if not only_exact:
+                data.append([A, b, volume])
+            
+            if check_exact_polytope(volume, A, b, m):
+                if r <= 3:
+                    plot_polytope(polytope, save=save_images, show=False, filename=path + "politope_" + str(i) + ".png")
+                
+                data_exact_politope.append([A, b, volume])
+                i += 1
         
         if i == 10 and save_images: 
             save_images = False
 
-    data = np.array(data, dtype=object)
-    np.save(path + "polytopes.npy", data)
+    if not only_exact:
+        data = np.array(data, dtype=object)
+    data_exact_politope = np.array(data_exact_politope, dtype=object)
     
-    return True
-
+    if not only_exact:
+        np.save(path + "all_polytopes.npy", data)
+    np.save(path + "exact_politopes.npy", data_exact_politope)
+    
 
 def load_data(filename):
     data = np.load(filename, allow_pickle=True)
@@ -79,22 +96,21 @@ def load_data(filename):
 
 
 def main():
-    m_array = range(3, 30)
-    r_array = range(2, 5)
-    n_polytopes = 50
+    m_array = [4, 5, 6] #range(3, 8)
+    r_array = [3, 4] #range(4, 5)
+    n_polytopes = 10000 #50
     seed = 0
     
     for r, m in product(r_array, m_array):
-        status = generate_n_polytopes(n_polytopes, base_path="./data/", seed=seed, m=m, r=r)
-        if not status:
-            print("Error in generating polytopes with m =", m, "and r =", r)
+        if m > r:
+            generate_n_polytopes(n_polytopes, base_path="./data/", seed=seed, m=m, r=r, only_exact=True)
+        else:
+            print("m must be greater than r")
 
 
 if __name__ == "__main__":
     #test_main()
     main()
-    
-    
 
 
 
